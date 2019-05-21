b2faf0590d5a9fa244af699bf79a53565f2ecb99
Rodrigo <isidro1984@gmail.com>
2019-05-21 23:48:52 +0300

    Removed the gulp not necessary items

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,7 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
  
-// gulp.task('default', function() {
-//   console.log('minifying js ...'); 
-//   return gulp.src('resources/sass/app.scss')
-//     .pipe(concat('crm.app.min.css'))
-//     .pipe(gulp.dest('public/css/'));
-// });
-
 //sass
 gulp.task('sass', function () {
     gulp.src(['resources/sass/app.scss'])
